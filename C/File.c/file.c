6f24a2aebbe9643406f483a292e7550424c0e819
SimArnHHH <simarnhhh@gmail.com>
2024-03-10 11:01:23 +0000

    Main

---
+++ C/File.c/file.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Ciao mondo!");
+    return 0;
+}
+
---
+++ CPP/ContatoreDoppio/ContatoreDoppio.cpp
@@ -0,0 +1,11 @@
+#include "ContatoreDoppio.h"
+
+void ContatoreDoppio::inc() {
+    Contatore::inc();
+    Contatore::inc();
+}
+
+void ContatoreDoppio::dec() {
+    Contatore::dec();
+    Contatore::dec();
+}
