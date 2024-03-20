a8d28ada4c6e19941b8c1f02858351566b8154a0
SimArnHHH <simarnhhh@gmail.com>
2024-03-20 12:31:04 +0000

    CodaConcatenata

---
+++ CPP/CodaConcatenata/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Coda.h"
+using namespace std;
+
+int main()
+{
+    Coda C1;
+
+    C1.push(5);
+    C1.push(7);
+    C1.push(6);
+    C1.pop();
+    C1.top();
+    C1.stampa();
+}
